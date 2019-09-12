c981afa24f6917ac268467e40c83220c2e69b72b
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-09-12 14:03:03 -0600

    Add project files.

---
+++ Find-The-Largest-Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_The_Largest_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+            int maxNum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Enter an integer: ");
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+                if (nums[i] > maxNum)
+                {
+                    maxNum = nums[i];
+                }
+            }
+
+            Console.WriteLine("The largest number that you entered is: " + maxNum);
+            Console.WriteLine("Press 'enter' to close the program.");
+            Console.ReadLine();
+        }
+    }
+}
